3a21475bef1570c2716e6bab6fa14750924ebd83
Andres Plazas <plazasmalagon@gmail.com>
2021-01-27 13:12:53 -0800

    Add needed files

---
+++ pixel_rejector.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from scipy.stats import norm
+from scipy import ndimage
+
+
+def estimateSigma(data = None, mu = 0., maxOrder = None, minOrder = None, n_iter = 1000):
+    factor = 0.
+    for i in xrange(n_iter):
+        sample =  np.random.randn(data.size) + mu
+        factor = factor + np.std(sample[minOrder:maxOrder])
+    factor = factor*1./n_iter
+    return factor * np.std(data[minOrder:maxOrder])
+
+
+def getBounds(data = None, center = None, thresh = 60,median = False):
+    from scipy.special import erf,erfc
+    from scipy.optimize import minimize
+    # First, get mode.
+    if center == None:
+        lo,hi = np.percentile(data,[10,90])
+        bins = np.linspace(lo,hi,151)
+        h,_ = np.histogram(data,bins)
+        ff = np.zeros(21)
+        ff[5:16] = 1./11.
+        yh = np.convolve(ff,h,mode='same')
+        maxind = np.argmax(yh)
+        mode = np.mean([bins[maxind],bins[maxind+1]])
+    elif median == False:
+        mode = center
+    else:
+        print "using the median to center the distribution"
+        mode = np.median(data)
+    print "assuming mode is:",mode
+    # Now assume that the data are gaussian around this point.
+    dist = data - mode
+    outer_bound_upper = np.percentile(np.abs(dist[dist > 0]),thresh)
+    outer_bound_lower = np.percentile(np.abs(dist[dist < 0]),thresh)
+    print "upper, lower bounds:", outer_bound_upper, outer_bound_lower
+    
+    window_bound = np.min([outer_bound_upper,outer_bound_lower])
+    these = ((data - mode) > -window_bound) & ((data-mode) < window_bound)
+    initial_sigma = (outer_bound_upper + outer_bound_lower)/2.
+    def gaussian_logL(sigma):
+        det =  np.sqrt(2*np.pi*sigma**2)
+        norm = 2./erf(window_bound/np.abs(sigma)/np.sqrt(2))
+        #logL_each = np.log(np.exp(-(data[these] - mode)**2/sigma**2/2.)/det * norm)
+        logL_each =-(data[these]-mode)**2/sigma**2/2. - np.log(det) + np.log(norm)
+        return -np.mean(logL_each)
+    res = minimize(gaussian_logL,initial_sigma)
+    sigma_est = np.abs(res.x[0])
+    ncore = 1./erf(window_bound/np.abs(sigma_est)/np.sqrt(2)) * np.sum(these)
+    
+    return sigma_est,ncore,mode,mode + outer_bound_upper,mode - outer_bound_lower
+
+def evaluate_logL(data, sigma=None, mu = None):
+    det =  np.sqrt(2*np.pi*sigma**2)
+    logL_each =-(data - mu)**2/sigma**2/2. - np.log(det)
+    return logL_each
+
+def evaluate_pdf(data, sigma = None, mu = None):
+    det =  np.sqrt(2*np.pi*sigma**2)
+    logL_each =-(data - mu)**2/sigma**2/2. - np.log(det)
+    L = np.exp(logL_each)
+    return L
+
+def get_test_data(mu = None, sigma = None):
+    data1 = sigma* np.random.randn(197000) + mu
+    data2 = .3*np.random.randn(3000) -3.
+    data = np.concatenate((data1,data2))
+    return data
+
+def get_real_data(dfile = './2017-01-13/master-dark.fits'):
+    data = fits.getdata(dfile).astype('float')
+    #data = data[data != 0]
+    return data
+
+def determine_quality_threshold(objval,bins,qhist, nkeep, forgive = None):
+    # return array of indices into original object array of those that pass quality cuts.
+    bin_inds = np.digitize(objval,bins)-1
+    
+    objQual = np.zeros_like(objval)
+    for ind in np.unique(bin_inds[(bin_inds > -1) & (bin_inds < qhist.size)] ):
+        objQual[bin_inds == ind] = qhist[ind]
+    if (np.min(bin_inds) == -1) | (np.max(bin_inds) >= qhist.size):
+        objQual[(bin_inds == -1) | (bin_inds >= qhist.size)] = np.max(qhist)
+    keep_frac = nkeep*100./objval.size
+    thresh = np.percentile(objQual,keep_frac)
+    if forgive is not None:
+        thresh = thresh*(1+forgive)
+    bad_inds = objQual > thresh
+    return thresh,bad_inds
+
+def find_anomalies(data,center  = None, forgive = .25):
+    sigma_est,ncore, mode, upper_bd, lower_bd = getBounds(data = data,thresh=80,median=False, center = center)
+    print "best sigma guess for sigma is:",sigma_est
+    print "number of core elements is:",ncore
+    dist = norm(loc=mode,scale=sigma_est)
+    low_bd,hi_bd = dist.interval(1 - 1./data.size)
+    bins = np.linspace(low_bd, hi_bd,150)
+    h,_ = np.histogram(data,bins=bins,density=True)
+    bin_centers = (bins[0:-1] + bins[1:])/2.
+    L = evaluate_pdf(bins, sigma = sigma_est, mu = mode)
+    Lmean = (L[0:-1] + L[1:])/2. * data.size
+    excess =  np.abs(h*data.size - Lmean)
+    excess_err = np.sqrt(Lmean)
+    exthresh,mask = determine_quality_threshold(data,bins,excess/excess_err, ncore,forgive=forgive)
+    mask_open = ndimage.binary_opening(mask)
+    mask_adj = ndimage.binary_closing(mask_open)
+
+    return mask, mask_adj
+    
+
+def main(argv):
+    sigma_true = 1.0
+    mu_true = 0.
+    forgive = 0.25
+    data = get_test_data(mu = mu_true, sigma = sigma_true)
+    #data = get_real_data()
+    sigma_est,ncore, mode, upper_bd, lower_bd = getBounds(data = data,thresh=80,median=False)
+    logL = evaluate_logL(data, sigma=sigma_est, mu = mode)
+    #print "bound is:",bd
+    print "best sigma guess, sigma actual is:",sigma_est, sigma_true
+    print "number of core elements is:",ncore
+
+    # Now that we have the model fit 
+    # and the log-likelihoods, figure 
+    # out which points don't belong.
+    # Which points have a low likelihood, 
+    # given all of the others?
+
+    from scipy.stats import norm
+    dist = norm(loc=mode,scale=sigma_est)
+    # thresh = 1./npts
+    low_bd,hi_bd = dist.interval(1 - 1./data.size)
+    print low_bd, hi_bd
+    bins = np.linspace(low_bd, hi_bd,150)
+    h,_ = np.histogram(data,bins=bins,density=True)
+    bin_centers = (bins[0:-1] + bins[1:])/2.
+    L = evaluate_pdf(bins, sigma = sigma_est, mu = mode)
+    Lmean = (L[0:-1] + L[1:])/2. * data.size
+    plt.plot(bin_centers, h*data.size)
+    plt.plot(bin_centers, Lmean)
+    plt.axvline(upper_bd,color='red',linestyle='--')
+    plt.axvline(lower_bd,color='red',linestyle='--')
+    plt.yscale('log')
+    plt.show()
+
+
+    # To establish the thresh, let's figure out how many objects we'll
+    # actually want to throw out.
+    ngood = ncore*1./data.size
+    # Let's assume that the Poisson error is 1./sqrt(n), 
+    # for n in the histogram bins.
+    excess =  np.abs(h*data.size - Lmean)
+    excess_err = np.sqrt(Lmean)
+    exthresh,bad_inds = determine_quality_threshold(data,bins,excess/excess_err, ncore)
+    exthresh = exthresh*(1+forgive)
+
+    mask = np.zeros_like(data).astype('int')
+    mask[bad_inds] = 1
+    # Let's try simplifying the mask.
+    from scipy import ndimage
+    mask_open = ndimage.binary_opening(bad_inds)
+    mask_close = ndimage.binary_closing(mask_open)
+    mask2 = np.zeros_like(data).astype('int')
+    mask2[mask_close] = 1
+    #mask[bad_inds & (data < mode)] = 2
+    #fits.writeto('dark_mask.fits',mask,clobber=True)
+    #fits.writeto('dark_mask2.fits',mask2,clobber=True)    
+    plt.plot(bin_centers, excess / excess_err)
+    plt.axhline(exthresh,color='red',linestyle='--')
+    plt.show()
+
+
+if __name__ == "__main__":
+    import pdb, traceback,sys
+    try:
+        main(sys.argv)
+    except:
+        thingtype, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
---
+++ sextractor_engine.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+#import subprocess
+#import sys
+#import time
+import os
+import copy
+
+#import utils
+#from defaults import *
+
+class DEFAULT:
+    IMAGE = 'NONE'
+    WEIGHT = 'NONE'
+    CHECK = 'NONE'
+    CHECK_NAME = 'NONE'
+
+    WORKING_DIR = '/'.join( os.path.realpath(__file__).split('/')[:-1] )
+    ASTRO_DIR = os.path.join( WORKING_DIR, 'astro_config' )
+    SEX_CONFIG = os.path.join( ASTRO_DIR, 'sextractor.config' )
+    SEX_PARAM = os.path.join( ASTRO_DIR, 'rrg.param' )
+    SEX_NNW = os.path.join( ASTRO_DIR, 'default.nnw' )
+    SEX_CONV = os.path.join( ASTRO_DIR, 'gauss_1.5_3x3.conv' )
+
+
+class SextractorEngine():
+    """Overview of SextractorEngine class:
+        This is an object for running sextractor. Most importantly, it contains a dictionary of the configuration parameters given to sextractor.
+        This dictionary is named SextractorEngine().config.  Editing the dictionary directly is one way to change the sextractor configuration.
+        Values in the dictionary override those specified in the config file itself.  A default call to SextractorEngine will load default
+        files for the sextractor config, param, nnw, and filter filters. These defaults are located in the astro_config directory packed with
+        this file.
+        
+        Technically SextractorEngine() can be called with no arguments, but the sextractor run will fail if you do not supply an image and a weight file.
+        The fields that can be supplied in the constructor are the sextractor config parameters which take file paths (and CHECKIMAGE_TYPE, which is related to CHECKIMAGE_NAMES).
+           IMAGE 
+           WEIGHT_IMAGE 
+           CATALOG_NAME 
+           c
+           PARAMETERS_NAME
+           STARNNW_NAME
+           FILTER_NAME
+           CHECKIMAGE_TYPE
+           CHECKIMAGE_NAME
+        However, any other parameters available to configure sextractor can also be modified simply by adding them to the object's config dictionary. 
+       
+        The image(s) to give to sextractor are normally only given on the command line (i.e. not in the config file). The dictionary keyword for it here is 'IMAGE'.
+        When editing the dictionary do not use any -'s in the keywords. 
+        For example when running 'sex image.fits -c sex.config -WEIGHT_IMAGE weight.fits', 
+        you would have SE.config['c'] = 'sex.config', SE.config['IMAGE'] = image.fits, SE.config['WEIGHT_IMAGE'] in the config dictionary
+
+        In the sextractor documenation sextractor keyword, value pairs in the config file are primarily upper case. I stuck with the same case conventions as sextractor, 
+        so formatting should be the same as in the sextractor documentation (even though this can be slightly annoying to type).
+
+        Methods of SextractorEngine:
+            Use the run() method to run sextractor. The optional logfile argument is where to write the sextractor logging output. By default, it goes
+            to the same directory as the catalog, stripping off .fits type extensions and appending _log.txt
+
+            auto_checkimage_name() is a convenience function for automatically populating the CHECKIMAGE_NAME field, based on the CHECKIMAGE_TYPE and current
+            image or catalog name. By default the output directory for the check files is the same as the catalogs, and the check files are named with the same
+            base naming scheme as the image, but with a _IMAGETYPE appended to the file, e.g. image_BACKGROUND.fits.  The optional named_by argument can either be
+            'image' or 'catalog' to choose if the base name is from the image or catalog file. The optional dir argument changes the output directory for
+            check files.  It can be a single directory or a list the same length as the number of check files.
+
+        A few notes:
+            If you change some config parameters in the dictionary, and then change the config file referenced in the dictionary, the values in the dictionary will still take
+            precedence over those in the (new) file.  Anything in the dictionary will become an explicit command line argument given to sextractor.
+            To avoid this, if you would like to start fresh, with a new config file, call the constructor, e.g.
+            SE = SextractorEngine( IMAGE=image.fits, WEIGHT_IMAGE=weight.fits, c=new_sex.config )
+            where you could also specificy different params file, etc. if you wanted.
+
+            Currently I do not have a way for changing the params (which will be output in the catalog), other than chaning the 'PARAMTERS_NAME' keyword.  I may add another way.
+
+    """
+
+    
+    ############ You should not need to used any of these functions. They are things which helping under the hood.
+    
+    def _catalog_name(self, image, catalog_name):
+        if catalog_name is None:
+            self.config['CATALOG_NAME'] = os.path.join( os.getcwd(), image.strip().split('/')[-1].replace('.fits','.cat.fits') )
+        else:
+            self.config['CATALOG_NAME'] = catalog_name
+
+    def _checkimage(self, checkimage_type, checkimage_name):        
+        if checkimage_type!='NONE':
+            self.config['CHECKIMAGE_TYPE'] = checkimage_type
+            if checkimage_name!='NONE':
+                self.config['CHECKIMAGE_NAME'] = checkimage_name
+            else:
+                self.auto_checkimage_name()
+
+    ################################################################################################3
+
+
+    def __init__(self, IMAGE=DEFAULT.IMAGE, WEIGHT_IMAGE=DEFAULT.WEIGHT, CATALOG_NAME=None, c=DEFAULT.SEX_CONFIG, PARAMETERS_NAME=DEFAULT.SEX_PARAM, STARNNW_NAME=DEFAULT.SEX_NNW, FILTER_NAME=DEFAULT.SEX_CONV, CHECKIMAGE_TYPE=DEFAULT.CHECK, CHECKIMAGE_NAME=DEFAULT.CHECK_NAME, setup=None):
+        #print locals()
+
+        self.config = {}
+        self.config['IMAGE'] = IMAGE
+        #self.config['WEIGHT_IMAGE'] = WEIGHT_IMAGE
+        self.config['c'] = c
+        self.config['PARAMETERS_NAME'] = PARAMETERS_NAME
+        self.config['STARNNW_NAME'] = STARNNW_NAME
+        self.config['FILTER_NAME'] = FILTER_NAME
+
+        self._catalog_name(IMAGE,CATALOG_NAME)
+        self._checkimage(CHECKIMAGE_TYPE, CHECKIMAGE_NAME)
+
+        self.path = 'sex'
+        self.setup = setup
+
+
+    def Path(self, path):
+        self.path = path
+
+
+    def auto_checkimage_name(self, dir=None, named_by='image'):
+        tlist = self.config['CHECKIMAGE_TYPE'].split(',')
+        for i in range(len(tlist)):
+            tlist[i] = '_' + tlist[i].strip().replace('-','m') + '.fits'
+
+        stripped_cat = self._strip('CATALOG_NAME', ['.cat.fits', '.fits'] )[-1]
+        stripped_image = self._strip('IMAGE', ['.fits']).split('/')[-1]
+        cat_dir = '/'.join( self.config['CATALOG_NAME'].split('/')[:-1] )
+        
+        if named_by=='image':
+            stripped = stripped_image
+        elif named_by=='catalog':
+            stripped = stripped_cat
+
+        for i in range(len(tlist)):
+            file = stripped + tlist[i]
+            if dir is None:
+                d = cat_dir
+            elif type(dir)==str:
+                d = dir
+            elif type(dir)==list:
+                d = dir[i]
+            tlist[i] = os.path.join(d,file)
+
+        nstr = ','.join(tlist)
+        self.config['CHECKIMAGE_NAME'] = nstr
+    
+
+    def run(self, msg=None):
+        args = [self.path, self.config['IMAGE']]
+        for key in self.config.keys():
+            if key=='IMAGE':
+                continue
+            else:
+                args.append( '-%s' %key )
+            args.append( str(self.config[key]) )
+   
+        
+        cmd = ' '.join(args)
+        os.system(cmd)
