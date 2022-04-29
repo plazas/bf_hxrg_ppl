f3fc140c1d85aa4943b39c24225b73ad685b5905
Andres A. Plazas Malagon <aplazas@astro.princeton.edu>
2022-04-29 15:40:04 -0500

    Add formula to compare spots and flats from C. Hirata

---
+++ spots_vs_flats_coeff_hirata.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+# Formula from Chris hirata
+def calculate_B_gaussian(sigma,aNN, aD):
+    const = 1./(2*np.sqrt(2)*sigma)
+    num = 2*np.sqrt(2)*np.exp(-1./(8*sigma**2))
+    den = np.sqrt(np.pi)*sigma*math.erf(const)
+    factor = (aNN + 0.5*(1 + (math.erf(3*const))/(math.erf(const)))*aD)
+
+    return -(num/den)*factor
+
+
+# Approximate an Airy with a gaussian of sigma ~ 0.42\Lambda*f-number
+
+# Fig. 5, Choi and Hirata
+ppm = 1.e-6
+aNN = 0.29*ppm
+aD = 0.038*ppm
+f_number = 8
+sigma_PSF_PPL = 0.42*1000*f_number
+
+
+B = calculate_B_gaussian(sigma_PSF_PPL, aNN, aD)
+
+print ("B", B)
+
+
+B0 = calculate_B_gaussian(sigma_PSF_PPL, aNN, 0.0)
+
+print ("B0 (no diagonal terms)", B0)
+
